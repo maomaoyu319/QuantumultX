2dc58481b551802058a6e543d377d4b24c8101b8
Maomaoyu0319 <70495718+Maomaoyu0319@users.noreply.github.com>
2021-04-30 08:40:33 +0800

    Create index.js

---
+++ Js/index.js
@@ -0,0 +1,16 @@
+//云函数入口,需要运行什么脚本自己改名字
+exports.main_handler = async (event, context, callback) => {
+try {
+delete require.cache [require.resolve ('./jddi fruit.js')];
+require ("./jddj_fruit.js');
+delete require.cache [require.resolve ('./jddj_bean.js')];
+require ("./jddj_bean.js');
+delete require.cache [require.resolve ('./jddj_fruit_collectWater.js')]; require ('./jddj fruit collectwater.js');
+delete require.cache [require.resolve ('./jddj plantBeans.js')];
+require ('./jddj_plantBeans.js');
+delete require.cache [require.resolve ('./jddj_getPoints.js')];
+require ('./jddj_getPoints.js');
+catch (e) {
+console.error (e)
+  }
+}
